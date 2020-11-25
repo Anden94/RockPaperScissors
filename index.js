ea94cf1e9a7c551e0e3ae0bc1e303d496326572e
Anden94 <nilsson.94@live.se>
2020-11-25 09:11:47 +0100

    update

--- index.js
+++ index.js
@@ -8,52 +8,52 @@ let computerScore =0;
     function playRound(player, computer) {
         if(player == "Rock"){
             if(computer == "Rock"){
-                alert("It's a draw!");
+                //alert("It's a draw!");
             }
             if(computer == "Paper"){
-                alert("You loose!");
+                //alert("You loose!");
                 computerScore++;
                 document.getElementById("computer-score").innerHTML = computerScore;
             }
             if(computer == "Scissor"){
-                alert("You win!");
+                //alert("You win!");
                 playerScore++;
                 document.getElementById("player-score").innerHTML = playerScore;
             }            
         }
         if(player == "Paper"){
             if(computer == "Paper"){
-                alert("It's a draw!");
+                //alert("It's a draw!");
             }
             if(computer == "Rock"){
-                alert("You win!");
+                //alert("You win!");
                 playerScore++;
                 document.getElementById("player-score").innerHTML = playerScore;
             }
             if(computer == "Scissor"){
-                alert("You loose!");
+                //alert("You loose!");
                 computerScore++;
                 document.getElementById("computer-score").innerHTML = computerScore;
             }            
         }
         if(player == "Scissor"){
             if(computer == "Scissor"){
-                alert("It's a draw!");
+                //alert("It's a draw!");
             }
             if(computer == "Rock"){
-                alert("You loose!");
+                //alert("You loose!");
                 computerScore++;
                 document.getElementById("computer-score").innerHTML = computerScore;
             }
             if(computer == "Paper"){
-                alert("You win!!");
+                //alert("You win!");
                 playerScore++;
                 document.getElementById("player-score").innerHTML = playerScore;
             }            
         }   
         console.log(playerScore, computerScore)           
         if(playerScore == 5){
-            console.log("You win the game!!");
+            //console.log("You win the game!!");
             playerScore =0;
             computerScore =0;
             document.getElementById("player-score").innerHTML = 0;
@@ -61,7 +61,7 @@ let computerScore =0;
             
         }  
         if(computerScore == 5){
-            console.log("You lose the game!");
+            //console.log("You lose the game!");
             playerScore =0;
             computerScore =0;
             document.getElementById("player-score").innerHTML = 0;
@@ -76,13 +76,15 @@ let computerScore =0;
         var items = ['Rock', 'Paper', 'Scissor'];
         var item = items[Math.floor(Math.random() * items.length)];
         console.log("Computer has " + item);
+        document.getElementById('computer-result').innerHTML = item;
         return item;                      
     }
 
     function userValue(){
-        $("input").click(function() {
-            const answer = this.value;
-            console.log("Player has " + answer);           
+        $("button").click(function() {
+            const answer = this.innerHTML;
+            console.log("Player has " + answer);      
+            document.getElementById('player-result').innerHTML = answer;     
             playRound(answer, computerPlay());
         });
     }
